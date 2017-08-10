7c62c31886c555b843be647a03fb36515b702fdc
hugo <hfernandez@neuquen.gov.ar>
2017-08-10 18:15:36 -0300

    usamos api para post y put y usamos el schema de profesional actual

--- config.ts
+++ config.ts
@@ -1,2 +1,9 @@
 export const xlsx = './files/profesionales.xlsx';
-
+export const algorithms = 'Levenshtein';
+// Estos pesos se deben a que no tenemos los datos de fecha de nacimiento y sexo en el excel
+export const weights = {
+    identity: 0.6,
+    name: 0.4,
+    gender: 0,
+    birthDate: 0
+};
--- profesionalesFromXlsToMongoDb.ts
+++ profesionalesFromXlsToMongoDb.ts
@@ -1,51 +1,89 @@
-// Imports
+// My Imports
 import * as configPrivate from './config.private.js'
 import * as config from './config'
-// Require
-var MongoClient = require('mongodb').MongoClient;
-var mongoXlsx: any = require('mongo-xlsx');
+import {
+    ProfesionalAndes
+} from './profesionalAndes'
+import {
+    matching
+} from '@andes/match';
 
-var model = null;
-let url = configPrivate.connectionString;
+// Imports 3rd Parties
+let mongoClient = require('mongodb').MongoClient;
+let mongoXlsx = require('mongo-xlsx');
+
+let model = null;
+let url = configPrivate.urlMongoAndes;
 let coleccion = configPrivate.collection;
+let profesionalOperations = new ProfesionalAndes();
+let unToken = 1;
 
-MongoClient.connect(url, function (err, db) {
+mongoClient.connect(url, function (err, db) {
     if (err) {
-        console.log('ERROR BD', err);
+        console.log('Error al conectarse a la base de datos', err);
         db.close();
     }
-
     mongoXlsx.xlsx2MongoData(config.xlsx, model, function (err, solapas) {
         solapas.forEach(datos => {
             if (datos.numeroDocumento) {
-            // Sólo importamos los que tengan documento y lo utilizo como clave para futuras actualizaciones con SAVE
-                let prof = {
-                    _id: datos.numeroDocumento,
+                let profXLS = {
                     documento: datos.numeroDocumento,
-                    apellido: datos.apellido,
                     nombre: datos.nombre,
-                    matricula: datos.matricula,
-                    legajo: datos.legajo,
-                    codigoSisa: datos.codigoSisa,
+                    apellido: datos.apellido,
+                    matriculas: [{
+                        numero: datos.matricula,
+                        descripcion: datos.especialidad
+                    }],
+                    legajo: datos.legajo ? datos.legajo : '',
+                    codigoSisa: datos.codigoSisa ? datos.sisa : '',
                     activo: datos.activo,
-                    telefono: datos.telefono,
-                    mail: datos.mail,
-                    especialidad: datos.especialidad
-                }
-
-                db.collection(coleccion).save(prof, function (err2, result) {
-                    if (err2) {
-                        console.log('ERROR ------------->:', err2);
-                        db.close();
-
+                    contacto: [{
+                            tipo: 'celular',
+                            valor: datos.telefono ? datos.telefono : '',
+                            activo: true
+                        },
+                        {
+                            tipo: 'email',
+                            valor: datos.mail ? datos.mail : '',
+                            activo: true
+                        }
+                    ],
+                };
+                // Condición de búsqueda para ver si el profesinal ya existe en la BD    
+                let condicion = {
+                    'documento': datos.numeroDocumento,
+                };
+                db.collection(coleccion).find(condicion).toArray(function (err, profesionalEncontrado: any) {
+                    if (profesionalEncontrado.length > 0) {
+                        let profDB = profesionalEncontrado[0];
+                        let match = new matching();
+                        let valorMatching = match.matchPersonas(profDB, profXLS, config.weights, config.algorithms);
+                        if (valorMatching > 0.9) {
+                            console.log('id', profDB._id);
+                            profesionalOperations.updateProfesional(profXLS, profDB._id)
+                                .then((rta: any) => {
+                                    console.log('Entro para hacer el update del paciente ', profXLS);
+                                })
+                        } else {
+                            profesionalOperations.addProfesional(profXLS)
+                                .then((rta: any) => {
+                                    console.log('Entro para hacer un insert del paciente ', profXLS);
+                                })
+                        }
                     } else {
-                        console.log('Se ha insertado:', prof);
-                        db.close();
-                    }
+                        // Si no encontró ningún registro directamente lo inserta en la BD
+                        profesionalOperations.addProfesional(profXLS)
+                            .then((rta: any) => {
+                                console.log('Entro para hacer un insert del paciente ', profXLS);
+                            })
 
-                });
+                    }
+                })
+
             }
-            
-        })
+
+        });
+        db.close();
     })
+
 })
