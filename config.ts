f834ec113b8529c7b2fc04caa2f2e5cf2efba9c6
hugo <hfernandez@neuquen.gov.ar>
2017-10-07 03:56:45 -0300

    refactoring

--- config.ts
+++ config.ts
@@ -2,8 +2,8 @@ export const xlsx = './files/profesionales2.xlsx';
 export const algorithms = 'Levenshtein';
 // Estos pesos se deben a que no tenemos los datos de fecha de nacimiento y sexo en el excel
 export const weights = {
-    identity: 0.6,
+    identity: 0.3,
     name: 0.4,
     gender: 0,
-    birthDate: 0
+    birthDate: 0.3
 };
--- profesionalesFromXlsToMongoDb.ts
+++ profesionalesFromXlsToMongoDb.ts
@@ -25,10 +25,12 @@ mongoClient.connect(url, function (err, db) {
     }
     mongoXlsx.xlsx2MongoData(config.xlsx, model, function (err, solapas) {
 
-        solapas.forEach(async datos => {
-            let p = new Promise(async(resolve, reject) => {
+        solapas.forEach(datos => {
+            let p = new Promise((resolve, reject) => {
 
                 let nombreCompleto = datos.Profesional.split(',');
+
+                // Creo el DTO de profesional
                 let profesional = {
                     documento: datos.Documento,
                     nombre: nombreCompleto[1],
@@ -59,50 +61,51 @@ mongoClient.connect(url, function (err, db) {
                     }],
                 }
 
-                let condicion = {'documento': profesional.documento};
-                db.collection(coleccion).find(condicion).toArray(async function (err, profesionalEncontrado: any) {
-                    if (profesionalEncontrado.length > 0) {
-                        console.log('entra para actualizar: ', profesionalEncontrado)
-                        let profDB = profesionalEncontrado[0];
-                        let match = new Matching();
-                        let valorMatching = await matchPacientes(profesional, profDB);
-                        console.log('dps del match ',valorMatching );
-                        if (valorMatching > 0.9) {
-                            console.log('antes del update');
-                            db.collection(coleccion).updateOne({
-                                _id: profDB._id
-                            }, {
+                let condicion = {
+                    'documento': datos.Documento
+                };
 
-                                $set: {
-                                    contacto: profesional.contacto,
-                                    matriculas: profesional.matriculas
-                                }
+                db.collection(coleccion).findOne(condicion)
+                    .then(async function (profesionalEncontrado) {
+                        console.log(profesionalEncontrado);
+                        if (profesionalEncontrado) {
+                            let match = new Matching();
+                            let valorMatching = await matchPacientes(profesional, profesionalEncontrado);
+                            if (valorMatching > 0.9) {
+                                db.collection(coleccion).updateOne({
+                                    _id: profesionalEncontrado._id
+                                }, {
 
-                            }, function (err, prof){
+                                    $set: {
+                                        contacto: profesional.contacto,
+                                        matriculas: profesional.matriculas
+                                    }
+
+                                }, function (err, prof) {
+                                    if (err) {
+                                        console.log(err);
+                                        reject(err);
+                                    } else {
+                                        resolve();
+                                    }
+                                });
+                            } else {
+                                // Si el matcheo es menor no hago nada
+                                resolve()
+                            }
+                        } else {
+                            // Si no existe en la bd lo inserta
+                            db.collection(coleccion).insertOne(profesional, function (err, prof) {
                                 if (err) {
-                                    console.log(err);
+                                    console.log('error', err);
                                     reject(err);
                                 } else {
                                     resolve();
                                 }
                             });
-                        } else {
-                            // Si el matcheo es menor no hago nada
-                            resolve()
                         }
-                    } else {
-                        // Si no existe en la bd lo inserta
-                        db.collection(coleccion).insertOne(profesional, function (err, prof) {
-                            if (err) {
-                                console.log('error', err);
-                                reject(err);
-                            } else
-                            {
-                                resolve();
-                            }
-                        });
-                    }
-                });
+                    })
+
             });
 
             arrayPromesas.push(p);
@@ -119,104 +122,32 @@ mongoClient.connect(url, function (err, db) {
 
 function matchPacientes(pacienteApp, pacienteVinculado) {
     return new Promise((resolve, reject) => {
-        let match = new Matching();
-        let pacApp = {
-            apellido: pacienteApp.apellido,
-            nombre: pacienteApp.nombre,
-            sexo: pacienteApp.sexo.toUpperCase(),
-            fechaNacimiento: pacienteApp.fechaNacimiento,
-            documento: pacienteApp.documento
-        };
-        let pac = {
-            apellido: pacienteVinculado.apellido,
-            nombre: pacienteVinculado.nombre,
-            sexo: pacienteVinculado.sexo.toUpperCase(),
-            fechaNacimiento: pacienteVinculado.fechaNacimiento,
-            documento: pacienteVinculado.documento
-        }
-        let valorMatching = match.matchPersonas(pacApp, pac, config.weights, 'Levenshtein');
-        if (valorMatching) {
-            resolve(valorMatching)
-        } else {
-            resolve(null)
+        try {
+            let match = new Matching();
+            let pacApp = {
+                apellido: pacienteApp.apellido,
+                nombre: pacienteApp.nombre,
+                sexo: pacienteApp.sexo.toUpperCase(),
+                fechaNacimiento: pacienteApp.fechaNacimiento,
+                documento: pacienteApp.documento
+            };
+            let pac = {
+                apellido: pacienteVinculado.apellido,
+                nombre: pacienteVinculado.nombre,
+                sexo: pacienteVinculado.sexo.toUpperCase(),
+                fechaNacimiento: pacienteVinculado.fechaNacimiento,
+                documento: pacienteVinculado.documento
+            }
+            let valorMatching = match.matchPersonas(pacApp, pac, config.weights, 'Levenshtein');
+            if (valorMatching) {
+                resolve(valorMatching)
+            } else {
+                resolve(null)
+            }
+        } catch (e) {
+            console.log('Error paso por catch', e);
+            resolve();
         }
     })
 };
 
-
-
-
-
-
-
-
-
-
-
-
-// if (datos.numeroDocumento) {
-//     let profXLS = {
-//         documento: datos.numeroDocumento,
-//         nombre: datos.nombre,
-//         apellido: datos.apellido,
-//         matriculas: [{
-//             numero: datos.matricula,
-//             descripcion: datos.especialidad
-//         }],
-//         legajo: datos.legajo ? datos.legajo : '',
-//         codigoSisa: datos.codigoSisa ? datos.sisa : '',
-//         activo: datos.activo,
-//         contacto: [{
-//                 tipo: 'celular',
-//                 valor: datos.telefono ? datos.telefono : '',
-//                 activo: true
-//             },
-//             {
-//                 tipo: 'email',
-//                 valor: datos.mail ? datos.mail : '',
-//                 activo: true
-//             }
-//         ],
-//     };
-//     // Condición de búsqueda para ver si el profesinal ya existe en la BD  
-
-//     let condicion = {
-
-//         'documento': datos.numeroDocumento,
-//     };
-//     db.collection(coleccion).find(condicion).toArray(function (err, profesionalEncontrado: any) {
-//         if (profesionalEncontrado.length > 0) {
-//             let profDB = profesionalEncontrado[0];
-//             let match = new matching();
-//             let valorMatching = match.matchPersonas(profDB, profXLS, config.weights, config.algorithms);
-//             // Tengo que convertir el valor numérico del documento a string
-//             profXLS.documento = String(profXLS.documento);
-//             if (valorMatching > 0.9) {
-//                 profesionalOperations.updateProfesional(profXLS, profDB._id)
-//                     .then((rta: any) => {
-//                         cantProfesionalesActualizados = cantProfesionalesActualizados + 1;
-//                         console.log('Entro para hacer el update del paciente ', cantProfesionalesActualizados, 'valor Match: ', valorMatching);
-//                     })
-//             } else {
-//                 profesionalOperations.addProfesional(profXLS)
-//                     .then((rta: any) => {
-//                         cantProfesionalesNuevoBajoMatch = cantProfesionalesNuevoBajoMatch + 1;
-//                         console.log('Entro para hacer un insert por bajo % ', cantProfesionalesNuevoBajoMatch, 'valor Match: ', valorMatching);
-//                     })
-//             }
-//         } else {
-//             // Si no encontró ningún registro directamente lo inserta en la BD
-
-//             // Tengo que convertir el valor numérico del documento a string
-//             profXLS.documento = String(profXLS.documento);
-
-//             profesionalOperations.addProfesional(profXLS)
-//                 .then((rta: any) => {
-//                     cantProfesionalesNuevos = cantProfesionalesNuevos + 1;
-//                     console.log('Entro para hacer un insert del paciente ', cantProfesionalesNuevos);
-//                 })
-
-//         }
-//     })
-
-// }
